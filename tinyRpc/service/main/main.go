27efed167414a1d5f75a56554a35fb132d1b525f
Eminem-x <1773997586@qq.com>
2023-04-12 16:08:30 +0800

    feat(service):补充测试demo

--- tinyRpc/service/main/main.go
+++ tinyRpc/service/main/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net"
 	"sync"
@@ -9,9 +8,31 @@ import (
 	"tinyRpc"
 )
 
+type Foo int
+
+type Args struct{ Num1, Num2 int }
+
+func (f Foo) Sum(args Args, reply *int) error {
+	*reply = args.Num1 + args.Num2
+	return nil
+}
+
+func startServer(addr chan string) {
+	var foo Foo
+	if err := tinyRpc.Register(&foo); err != nil {
+		log.Fatal("register error:", err)
+	}
+	// pick a free port
+	l, err := net.Listen("tcp", ":0")
+	if err != nil {
+		log.Fatal("network error:", err)
+	}
+	log.Println("start rpc server on", l.Addr())
+	addr <- l.Addr().String()
+	tinyRpc.Accept(l)
+}
+
 func main() {
-	// 设置选项可在每条输出的文本前增加一些额外信息，如日期时间、文件名等
-	// 可以参考: https://darjun.github.io/2020/02/07/godailylib/log/
 	log.SetFlags(0)
 	addr := make(chan string)
 	go startServer(addr)
@@ -25,25 +46,13 @@ func main() {
 		wg.Add(1)
 		go func(i int) {
 			defer wg.Done()
-			args := fmt.Sprintf("tinyRpc req %d", i)
-			var reply string
+			args := &Args{Num1: i, Num2: i * i}
+			var reply int
 			if err := client.Call("Foo.Sum", args, &reply); err != nil {
 				log.Fatal("call Foo.Sum error:", err)
 			}
-			log.Println("reply:", reply)
+			log.Printf("%d + %d = %d", args.Num1, args.Num2, reply)
 		}(i)
 	}
 	wg.Wait()
-}
-
-func startServer(addr chan string) {
-	// pick a free port
-	l, err := net.Listen("tcp", ":0")
-	if err != nil {
-		log.Fatal("network error:", err)
-	}
-	log.Println("start rpc server on", l.Addr())
-	// 使用信道 addr, 确保服务端端口监听成功, 客户端再发起请求
-	addr <- l.Addr().String()
-	tinyRpc.Accept(l)
 }
