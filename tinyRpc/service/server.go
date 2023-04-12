914b6700d2416f9988ce1167f26aa032e5346310
Eminem-x <1773997586@qq.com>
2023-04-12 15:12:25 +0800

    copy(service):准备增加服务注册功能

---
+++ tinyRpc/service/server.go
@@ -0,0 +1,152 @@
+package tinyRpc
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"reflect"
+	"sync"
+	"tinyRpc/codec"
+)
+
+const MagicNumber = 0x3bef5c
+
+type Option struct {
+	MagicNumber int        // MagicNumber marks this is s a tinyRpc request
+	CodecType   codec.Type // client may choose different Codec to encode body
+}
+
+var DefaultOption = &Option{
+	MagicNumber: MagicNumber,
+	CodecType:   codec.GobType,
+}
+
+// Server represents an RPC server
+type Server struct{}
+
+// NewServer returns a new Server
+func NewServer() *Server {
+	return &Server{}
+}
+
+// DefaultServer is the default instance of *Server
+var DefaultServer = NewServer()
+
+// Accept accepts connections on the listener and serves requests for each incoming connection.
+func Accept(lis net.Listener) { DefaultServer.Accept(lis) }
+
+// Accept accepts connections on the listener and serves requests for each incoming connection.
+func (server *Server) Accept(lis net.Listener) {
+	// for 循环等待 socket 连接建立, 并开启子协程处理, 处理过程交给了 ServerConn 方法。
+	for {
+		conn, err := lis.Accept()
+		if err != nil {
+			log.Println("rpc server: accept error:", err)
+			return
+		}
+		go server.ServeConn(conn)
+	}
+}
+
+// ServeConn runs the server on a single connection.
+// ServeConn blocks, serving the connection until the client hangs up.
+func (server *Server) ServeConn(conn io.ReadWriteCloser) {
+	defer func() { _ = conn.Close() }()
+
+	var opt Option
+	// 1. 使用 json.NewDecoder 反序列化得到 Option 实例
+	if err := json.NewDecoder(conn).Decode(&opt); err != nil {
+		log.Println("rpc server: options error: ", err)
+		return
+	}
+
+	// 2. 检查 MagicNumber 和 CodeType 的值是否正确
+	if opt.MagicNumber != MagicNumber {
+		log.Printf("rpc server: invalid magic number %x", opt.MagicNumber)
+		return
+	}
+
+	// 3. 根据 CodeType 得到对应的消息编解码器
+	f := codec.NewCodecFuncMap[opt.CodecType]
+	if f == nil {
+		log.Printf("rpc server: invalid codec type %s", opt.CodecType)
+		return
+	}
+
+	// 4. 交给 serverCodec 处理
+	server.serveCodec(f(conn))
+}
+
+// invalidRequest is a placeholder for response argv when error occurs
+var invalidRequest = struct{}{}
+
+func (server *Server) serveCodec(cc codec.Codec) {
+	sending := new(sync.Mutex)
+	wg := new(sync.WaitGroup)
+	for {
+		// 1. 读取请求
+		req, err := server.readRequest(cc)
+		if err != nil {
+			if req == nil {
+				break // it's not possible to recover, so close the connection
+			}
+			req.h.Error = err.Error()
+			server.sendResponse(cc, req.h, invalidRequest, sending)
+			continue
+		}
+		wg.Add(1)
+		// 2. 处理 & 回复请求
+		go server.handleRequest(cc, req, sending, wg)
+	}
+	wg.Wait()
+	_ = cc.Close()
+}
+
+// request stores all information of a call
+type request struct {
+	h            *codec.Header // header of request
+	argv, replyv reflect.Value // argv and replyv of requests
+}
+
+func (server *Server) readRequest(cc codec.Codec) (*request, error) {
+	h, err := server.readRequestHeader(cc)
+	if err != nil {
+		return nil, err
+	}
+	req := &request{h: h}
+	// just suppose it is string, which will enhanced in the future
+	req.argv = reflect.New(reflect.TypeOf(""))
+	if err = cc.ReadBody(req.argv.Interface()); err != nil {
+		log.Println("rpc server: read argv err:", err)
+	}
+	return req, nil
+}
+
+func (server *Server) readRequestHeader(cc codec.Codec) (*codec.Header, error) {
+	var h codec.Header
+	if err := cc.ReadHeader(&h); err != nil {
+		if err != io.EOF && err != io.ErrUnexpectedEOF {
+			log.Println("rpc server: read header error:", err)
+		}
+		return nil, err
+	}
+	return &h, nil
+}
+
+func (server *Server) handleRequest(cc codec.Codec, req *request, sending *sync.Mutex, wg *sync.WaitGroup) {
+	// just print argv and send a hello message
+	defer wg.Done()
+	log.Println(req.h, req.argv.Elem())
+	req.replyv = reflect.ValueOf(fmt.Sprintf("tinyRpc resp %d", req.h.Seq))
+	server.sendResponse(cc, req.h, req.replyv.Interface(), sending)
+}
+
+func (server *Server) sendResponse(cc codec.Codec, h *codec.Header, body interface{}, sending *sync.Mutex) {
+	sending.Lock()
+	defer sending.Unlock()
+	if err := cc.Write(h, body); err != nil {
+		log.Println("rpc server: write response error:", err)
+	}
+}
