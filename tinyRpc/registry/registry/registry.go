91bafdf125330a5cb29c37e0ec1492b454d4192a
Eminem-x <1773997586@qq.com>
2023-04-28 15:58:45 +0800

    feat(registry): complete registry module

--- tinyRpc/registry/registry/registry.go
+++ tinyRpc/registry/registry/registry.go
@@ -1,1 +1,125 @@
 package registry
+
+import (
+	"log"
+	"net/http"
+	"sort"
+	"strings"
+	"sync"
+	"time"
+)
+
+// Registry is a simple register center, provide following functions.
+// add a server and receive heartbeat to keep it alive.
+// returns all alive servers and delete dead servers sync simultaneously
+type Registry struct {
+	timeout time.Duration
+	mu      sync.Mutex // protect following
+	servers map[string]*ServerItem
+}
+
+type ServerItem struct {
+	Addr  string
+	start time.Time
+}
+
+const (
+	defaultPath    = "/_tinyRpc/registry"
+	defaultTimeout = time.Minute * 5
+)
+
+// New create a registry instance with timeout setting
+func New(timeout time.Duration) *Registry {
+	return &Registry{
+		servers: make(map[string]*ServerItem),
+		timeout: timeout,
+	}
+}
+
+var DefaultRegistry = New(defaultTimeout)
+
+func (r *Registry) putServer(addr string) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	s := r.servers[addr]
+	if s == nil {
+		r.servers[addr] = &ServerItem{Addr: addr, start: time.Now()}
+	} else {
+		s.start = time.Now() // is exists, update start time to keep alive
+	}
+}
+
+func (r *Registry) aliveServers() []string {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	var alive []string
+	for addr, s := range r.servers {
+		if r.timeout == 0 || s.start.Add(r.timeout).After(time.Now()) {
+			alive = append(alive, addr)
+		} else {
+			delete(r.servers, addr)
+		}
+	}
+	sort.Strings(alive)
+	return alive
+}
+
+// ServeHTTP runs at /_tinyRpc_/registry
+func (r *Registry) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case "GET":
+		// keep it simple, server is in req.Header
+		// 返回所有可用的服务列表，通过自定义字段 X-TinyRpc-Servers 承载
+		w.Header().Set("X-TinyRpc-Servers", strings.Join(r.aliveServers(), ","))
+	case "POST":
+		// keep it simple, server is in req.Header
+		// 添加服务实例或发送心跳，通过自定义字段 X-TinyRpc-Server 承载
+		addr := req.Header.Get("X-TinyRpc-Server")
+		if addr == "" {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		r.putServer(addr)
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
+func (r *Registry) HandleHTTP(registryPath string) {
+	http.Handle(registryPath, r)
+	log.Println("rpc registry path: ", registryPath)
+}
+
+func HandleHTTP() {
+	DefaultRegistry.HandleHTTP(defaultPath)
+}
+
+// Heartbeat send a heartbeat message every once in a while
+// it's a helper function for a server to register or send heartbeat
+func Heartbeat(registry, addr string, duration time.Duration) {
+	if duration == 0 {
+		// make sure there is enough time to send heart beat before it's removed from registry
+		duration = defaultTimeout - time.Duration(1)*time.Minute
+	}
+	var err error
+	err = sendHeartbeat(registry, addr)
+	go func() {
+		t := time.NewTicker(duration)
+		for err == nil {
+			<-t.C
+			err = sendHeartbeat(registry, addr)
+		}
+	}()
+}
+
+func sendHeartbeat(registry, addr string) error {
+	log.Println(addr, "send heart beat to registry", registry)
+	httpClient := &http.Client{}
+	req, _ := http.NewRequest("POST", registry, nil)
+	req.Header.Set("X-TinyRpc-Server", addr)
+	if _, err := httpClient.Do(req); err != nil {
+		log.Println("rpc server: heart beat err: ", err)
+		return err
+	}
+	return nil
+}
